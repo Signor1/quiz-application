f185f64b2376443c747a6f93f9dadf2fb7194108
signor1 <emmanuelomemgboji@gmail.com>
2023-12-16 18:41:31 +0100

    feat: integrations

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,9 +1,42 @@
+import { useRef, useState } from 'react'
 
+import useSound from "use-sound";
+import game from "../assets/main.mp3";
 
-const Start = () => {
+export default function Start({ setUserName, userName }) {
+    //error msg
+    const [error, setError] = useState(false);
+    //inputRef
+    const inputRef = useRef()
+
+    const [playGame, { stop }] = useSound(game);
+
+    const handleClick = () => {
+        //if the input is empty set error to true else, set the value to the username
+        if (inputRef.current.value === "") {
+            setError(true);
+        } else {
+            //stop the sound
+            stop()
+            setUserName(inputRef.current.value);
+        }
+
+    }
     return (
-        <div>Start</div>
+        <div className='start'>
+
+            <div className="content" >
+                <div className="wrapper">
+                    <label>Enter Your Username To Start </label>
+                    <input type="text" placeholder='Enter Your Username' className='startInput' ref={inputRef} onFocus={() => playGame()} />
+                    {
+                        error && <code>Enter username!</code>
+                    }
+                    <div className="btn">
+                        <button className='startButton' onClick={handleClick}>Start Quiz</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
-
-export default Start
